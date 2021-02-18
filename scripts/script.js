a2e041c807d1b11684a142ef3c60fcf17d40a61c
Rafael <rafaelnepomuceno00@gmail.com>
2021-02-18 18:19:38 -0300

    Layout and using api

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,102 @@
-const Row = document.querySelector('#row')
-const url = 'https://pokeapi.co/api/v2/pokemon'
-const pokemons = []
-const previous = null
-function getPokemons(){
+let Row = document.getElementById('row')
+let page1 = 'https://pokeapi.co/api/v2/pokemon'
+let pokemons = []
+let next
+let previous
+let Prev = document.getElementById('prev')
+let Next = document.getElementById('next')
 
-    fetch(url).then((response)=>{
-    if(!response){
-        throw Error("Error at response")
-    }        else{
-        return response.json()
-    }
-    }).then(data=>{
-       array = data.results 
-       array.forEach(element => {
-       });
-       return data
+
+Next.addEventListener('click', function () {
+    getAllPokemons(next)
+    createAllCards(pokemons)
+    setLinks()
+})
+Prev.addEventListener('click', function () {
+    getAllPokemons(previous)
+    createAllCards(pokemons)
+    setLinks()
+})
+
+getAllPokemons(page1)
+window.onload = function () {
+    createAllCards(pokemons)
+    setLinks()
+
+}
+
+
+function getAllPokemons(url) {
+ 
+    fetch(url).then((response) => {
+        if (!response) {
+            throw Error("Error at response")
+        } else {
+            return response.json()
+        }
+    }).then(data => {
+        next = data.next == undefined ? null : data.next
+        previous = data.previous == undefined ? null : data.previous
+        array = data.results
+        array.forEach(element => {
+            pokemons.push(element)
+        });
+
     })
 
 }
-console.log(getPokemons())
+function createAllCards(pokemons) {
 
-    // < div id = "row" class="row row-cols-4 justify-content-between" >
-    //     <div class="card mb-5" style="width: 300px;">
-    //         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-    //             class="card-img-top">
-    //             <div class="card-body">
-    //                 <h5 class="card-title text-center">
-    //                     bulbasaur
-    //                     </h5>
-    //             </div>
-    //         </div>
-    //     </div>
+    let i = 1
+    pokemons.forEach(pokemon => {
+
+        let card = document.createElement('div')
+        card.classList.add('card')
+        card.classList.add('mb-5')
+        card.style.width = '300px'
+
+        let imgCard = document.createElement('img')
+        imgCard.classList.add('card-img-top')
+        imgCard.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + i + '.png'
+        card.appendChild(imgCard)
+
+        let cardBody = document.createElement('div')
+        cardBody.classList.add('card-body')
+        card.appendChild(cardBody)
+        let title = document.createElement('h5')
+        title.classList.add('card-title')
+        title.classList.add('text-center')
+        title.classList.add('text-capitalize')
+        title.innerHTML = pokemon.name
+        cardBody.appendChild(title)
+
+        Row.appendChild(card)
+        i++
+    })
+}
+
+
+function setLinks() {
+
+    let PrevToggler = document.getElementById('prev-toggle')
+    let NextToggler = document.getElementById('next-toggle')
+
+    previous == null ? PrevToggler.classList.add('disabled') : PrevToggler.classList.remove('disabled')
+    next == null ? NextToggler.classList.add('disabled') : NextToggler.classList.remove('disabled')
+
+}
+
+function clearPage() {
+    pokemons = []
+    Row.innerText = ' '
+}
+// <nav aria-label="...">
+// <ul class="pagination justify-content-center">
+//     <li class="page-item disabled ">
+//         <a class="page-link alert-primary" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+//     </li>
+//     <li class="page-item">
+//         <a class="page-link alert-primary" href="#">Próximo</a>
+//     </li>
+// </ul>
+// </nav>
